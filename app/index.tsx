3641ee2d3d953212e6ec187c38c5ae7e3ad5ca5c
chicoumbetov <shyngysmaaan@gmail.com>
2023-10-07 16:11:02 +0200

    :sparkles: main page use all comps

---
+++ app/index.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Platform, ScrollView, StyleSheet, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Categories from "../components/Categories";
+import Restaurants from "../components/Restaurants";
+import Colors from "../constants/Colors";
+
+const Page = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView contentContainerStyle={{ paddingBottom: 40 }}>
+        <Categories />
+        <Text style={styles.header}>Top picks in your neighbourhood</Text>
+        <Restaurants />
+        <Text style={styles.header}>Offers near you</Text>
+        <Restaurants />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    top: Platform.OS === "web" ? 0 : 70,
+    backgroundColor: Colors.lightGrey,
+  },
+  header: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginTop: 16,
+    marginBottom: 8,
+    paddingHorizontal: 16,
+  },
+});
+
+export default Page;
